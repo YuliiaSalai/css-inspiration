15de369b28d8c0c7c321c55613923fd1498f73f1
YuliiaSalai <83586009+YuliiaSalai@users.noreply.github.com>
2021-05-14 21:30:22 +0300

    Project commit

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+let btn = document.getElementsByTagName('button')[0];
+let modal = document.getElementsByClassName('modal')[0];
+btn.addEventListener('click',()=>{
+    modal.classList.toggle('hidden');
+    setTimeout(function(){
+        location='./3D card.html'}
+        , 7500);
+});
